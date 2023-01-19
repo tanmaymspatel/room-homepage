a92cc085a58cbc4298d8ba6b90b191a8d85fc708
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-19 17:26:03 +0530

    basic structure of about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-
+import RoomHomepage from "./pages/RoomHomepage";
 
 function App() {
   return (
-    <div className="h-100 d-flex flex-column">
-      <div className="bg-primary flex-grow-1"></div>
-      <div className="about bg-secondary"></div>
-    </div>
+    <RoomHomepage />
   );
 }
 
