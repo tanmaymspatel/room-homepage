8ca693790f227d149ca16d5a9e1e99c3b91418f6
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-20 18:02:53 +0530

    carousel responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import lightImg from '../assets/images/image-about-light.jpg'
 function About() {
     return (
         <>
-            <div className="row g-0">
+            <div className="row g-0 h-100">
                 <div className="about-image-col">
                     <figure className='mb-0 h-100 w-100'>
                         <img src={darkImg} alt="about-dark" className='h-100 w-100' />
@@ -12,7 +12,7 @@ function About() {
                 </div>
                 <div className="about-col">
                     <div className='about-content h-100 d-flex flex-column justify-content-center'>
-                        <h4 className='heading-about text-uppercase pb-1'>about our furniture</h4>
+                        <h4 className='heading-about text-uppercase pb-1 text-nowrap'>about our furniture</h4>
                         <p className='about-text text-primary'>
                             Our multifunctional collection blends design and function to suit your
                             individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
--- src/components/Slide3.tsx
+++ src/components/Slide3.tsx
@@ -1,17 +1,19 @@
 import imgSlider3 from '../assets/images/desktop-image-hero-3.jpg'
+import mobileImgSlider3 from '../assets/images/mobile-image-hero-3.jpg'
 import shopArrow from '../assets/images/icon-arrow.svg'
 
 function Slide3() {
     return (
         <div className="row g-0 h-100">
             <div className="col-lg-7">
-                <img src={imgSlider3} alt="hero-3" className='w-100 h-100' />
+                <img src={imgSlider3} alt="hero-3" className='w-100 h-100 d-none d-sm-block' />
+                <img src={mobileImgSlider3} alt="hero-3" className='w-100 h-100 d-sm-none' />
             </div>
             <div className="col-lg-5">
-                <div className='hero-content h-100 d-flex flex-column justify-content-center'>
+                <div className='hero-content d-flex flex-column justify-content-center'>
                     <h1 className='hero-heading'>Manufactured with the best materials</h1>
-                    <p className='text-primary'>Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.</p>
-                    <p className='py-2 cursor-pointer'>
+                    <p className='text-primary hero-text'>Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.</p>
+                    <p className='pt-3 cursor-pointer mb-0'>
                         <span className='text-uppercase me-3 shop-text transition'>shop now</span>
                         <img className='shop-arrow' src={shopArrow} alt="shop-now" />
                     </p>
